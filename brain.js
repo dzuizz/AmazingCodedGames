705cae3c9b271c4ee14ef73b2a537d7fd0d78399
dzuizz <59071340+dzuizz@users.noreply.github.com>
2020-08-20 17:00:59 +0800

    updating the image for the top of a web (introduction)

--- brain.js
+++ brain.js
@@ -1,11 +1,11 @@
-// colorPicker
+// Select color input
 var color = document.getElementById('colorPicker').value;
-// submition
+// Select size input
 var width = document.getElementById('inputWidth').value;
 var height = document.getElementById('inputHeight').value;
 const submit = document.getElementById("submitB");
 
-// When size is submitted by user, call MakeGrid()
+// When size is submitted by the user, call makeGrid()
 sizePicker.addEventListener('submit', function() {
   event.preventDefault();
   width = document.querySelector('#inputWidth').value;
@@ -13,7 +13,8 @@ sizePicker.addEventListener('submit', function() {
   makeGrid(height, width);
 })
 
-function makeGrid() {
+function makeGrid(h, w) {
+// Your code goes here!
   var table = document.getElementById('pixelCanvas')
   table.innerHTML = "";
   for (var x = 0 ; x < h ; x++) {
