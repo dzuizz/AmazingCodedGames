d331d221f681c9ba624321170583a3f4601c543c
dzuizz <59071340+dzuizz@users.noreply.github.com>
2020-08-21 19:04:10 +0800

    Updating the meme generator

--- pixelGameBrain.js
+++ pixelGameBrain.js
@@ -1,3 +1,18 @@
+// Select color input
+var color = document.getElementById('colorPicker').value;
+// Select size input
+var width = document.getElementById('inputWidth').value;
+var height = document.getElementById('inputHeight').value;
+const submit = document.getElementById("submitB");
+
+// When size is submitted by the user, call makeGrid()
+sizePicker.addEventListener('submit', function() {
+  event.preventDefault();
+  width = document.querySelector('#inputWidth').value;
+  height = document.querySelector('#inputHeight').value;
+  makeGrid(height, width);
+})
+
 function makeGrid(h, w) {
 // Your code goes here!
   var table = document.getElementById('pixelCanvas')
